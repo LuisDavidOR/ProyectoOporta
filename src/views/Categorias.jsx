2d27efe956130d3e1f1a5e4d8f85e0e5e1e18e13
LuisDavidOR <luisdavidoporta777@gmail.com>
2026-04-20 16:01:30 -0600

    Practica de edicion y eliminacion de categorias

--- src/views/Categorias.jsx
+++ src/views/Categorias.jsx
@@ -6,6 +6,9 @@ import ModalRegistroCategoria from "../components/categorias/ModalRegistroCatego
 import NotificacionOperacion from "../components/NotificacionOperacion";
 import TablaCategorias from "../components/categorias/TablaCategorias";
 import TarjetaCategoria from "../components/categorias/TarjetaCategoria";
+import ModalEdicionCategoria from "../components/categorias/ModalEdicionCategoria";
+import ModalEliminacionCategoria from "../components/categorias/ModalEliminacionCategoria";
+
 
 const Categorias = () => {
 
@@ -36,6 +39,14 @@ const Categorias = () => {
       descripcion_categoria: categoria.descripcion_categoria,
     });
     setMostrarModalEdicion(true);
+  };
+
+  const manejoCambioInputEdicion = (e) => {
+    const { name, value } = e.target;
+    setCategoriaEditar((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
   };
 
   const abrirModalEliminacion = (categoria) => {
@@ -136,6 +147,92 @@ const Categorias = () => {
     }
   };
 
+  const actualizarCategoria = async () => {
+    try {
+      if (
+        !categoriaEditar.nombre_categoria.trim() ||
+        !categoriaEditar.descripcion_categoria.trim()
+      ) {
+        setToast({
+          mostrar:true,
+          mensaje: "Debe llenar todos los campos.",
+          tipo: "advertencia",
+        });
+        return;
+      }
+
+      setMostrarModalEdicion(false);
+
+      const { error } = await supabase
+        .from("Categorias")
+        .update({
+          nombre_categoria: categoriaEditar.nombre_categoria,
+          descripcion_categoria: categoriaEditar.descripcion_categoria,
+        })
+        .eq("id_categoria", categoriaEditar.id_categoria);
+
+        if (error) {
+          console.error("Error al actualizar categoría:", error.message);
+          setToast({
+            mostrar: true,
+            mensaje: `Error al actualizar la categoría ${categoriaEditar.nombre_categoria}.`,
+            tipo: "error",
+          });
+          return;
+        }
+
+        await cargarCategorias();
+        setToast({
+          mostrar: true,
+          mensaje: `Categoría ${categoriaEditar.nombre_categoria} actualizada exitosamente.`,
+          tipo: "exito",
+        });
+    } catch (err) {
+      setToast({
+        mostrar: true,
+        mensaje: "Error inesperado al actualizar categoría.",
+        tipo: "error",
+      });
+      console.error("Excepción al actualizar categoría: ", err.message);
+    }
+  };
+
+  const eliminarCategoria = async () => {
+    if (!categoriaAEliminar) return;
+    try {
+      setMostrarModalEliminacion(false);
+
+      const { error } = await supabase
+        .from("Categorias")
+        .delete()
+        .eq("id_categoria", categoriaAEliminar.id_categoria);
+      
+      if (error) {
+        console.error("Error al eliminar categoría: ", error.message);
+        setToast({
+          mostrar: true,
+          mensaje: `Error al eliminar la categoría ${categoriaAEliminar.nombre_categoria}.`,
+          tipo: "error",
+        });
+        return;
+      }
+
+      await cargarCategorias();
+      setToast({
+        mostrar: true,
+        mensaje: `Categoría ${categoriaAEliminar.nombre_categoria} eliminada exitosamente.`,
+        tipo: "exito",
+      });
+    } catch (err) {
+      setToast ({
+        mostrar: true,
+        mensaje: "Error inesperado al eliminar categoría.",
+        tipo: "error",
+      });
+      console.error("Excepción al eliminar categoría: ", err.message);
+    }
+  };
+
   return (
     <Container className="mt-3">
       <Row className="align-items-center mb-3">
@@ -193,6 +290,21 @@ const Categorias = () => {
         nuevaCategoria={nuevaCategoria}
         manejoCambioInput={manejoCambioInput}
         agregarCategoria={agregarCategoria}
+      />
+
+      <ModalEdicionCategoria
+        mostrarModalEdicion={mostrarModalEdicion}
+        setMostrarModalEdicion={setMostrarModalEdicion}
+        categoriaEditar={categoriaEditar}
+        manejoCambioInputEdicion={manejoCambioInputEdicion}
+        actualizarCategoria={actualizarCategoria}
+      />
+
+      <ModalEliminacionCategoria
+        mostrarModalEliminacion={mostrarModalEliminacion}
+        setMostrarModalEliminacion={setMostrarModalEliminacion}
+        eliminarCategoria={eliminarCategoria}
+        categoria={categoriaAEliminar}
       />
 
       <NotificacionOperacion 
